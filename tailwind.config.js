3f1e6e35510451d6d216cd61cd9bdd1f52db2dc4
jinsumin <suminjin@naver.com>
2022-08-03 17:54:03 +0900

    init setting

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,tx,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
